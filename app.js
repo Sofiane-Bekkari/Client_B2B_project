8ab1e157f4f82d9f6f900c47053903f2804adda9
Sofiane Bekkari <sofhill.ss@gmail.com>
2021-01-22 11:12:02 +0100

    lamp scroll in section 2

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
+//*** Berger Menu ***//
 const berger = document.querySelector('.berger');
 const shadow = document.querySelector('.shadow');
 const navBar = document.querySelector('.nav-bar');
 
-
 // Event for berger menu
 berger.addEventListener('click', function(){
-    console.log('Clickable!')
-    navBar.classList.toggle('show-nav')
-    shadow.classList.toggle('shadow')
-
+    navBar.classList.toggle('show-nav');
 })
 
-// Section Observer
-const titleFade = document.querySelector('slider-title'); 
+
+///**** lamp light ****/// 
+let lamp = document.querySelector('.light-lamp');
+
+console.log(lamp)
+
+window.addEventListener('scroll', function(){
+    let scroll = window.pageYOffset
+    console.log("Here scroll",scroll);
+    calcul = scroll - 150
+    let topBackZero = -180
+    console.log("Here calcul ",calcul);
+    if (scroll > 50){
+        console.log('Is Big');
+        lamp.style.top = `${calcul}px`;
+    } else if (scroll < 130) {
+        lamp.style.top = `${topBackZero}px`; 
+        console.log('Is Less', topBackZero);
+    }
+
+    // Check if it gearter to fix it
+    if  (scroll > 350) {
+        lamp.style.top = `10px`;
+        lamp.style.opacity = 0.5;
+        console.log('yes is greater')
+        
+    } else {
+
+        lamp.style.opacity = 0.9;
+        lamp.style.transform = `translateX(0)`;
+    }
+
+    // Check to translate it left
+    if (scroll > 380){
+        lamp.style.top = `200px`;
+        lamp.style.transform = `translateX(-2000%)`;
+    }
+
+});
