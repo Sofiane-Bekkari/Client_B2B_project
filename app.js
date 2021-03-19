7e56ecabf3604a6f8c61bc0f2af4bdcbebabc0e5
Sofiane Bekkari <sofhill.ss@gmail.com>
2021-03-19 21:59:28 +0100

    Added a scrolling after light

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ berger.addEventListener('click', function(){
 let lamp = document.querySelector('.light-lamp');
 const showcaseWidth = document.querySelector('.showcase');
 const lampDark = document.querySelector('.lamp-dark-1');
-console.log(lampDark, 'HERE§§')
+const yellowLamp = document.querySelector(".lamp-yellow");
 const lightTime = document.querySelector('.spot-light');
 const shadowLight = document.querySelector('img.lamp-shadow');
 
@@ -54,35 +54,113 @@ window.addEventListener('scroll', function(){
     // Cheching if time to lighting            
     if (width > 900){
         timeLighting();
+        continueScroll();
+        yellowCheck();
 
     }
 
     // Cheching light on 1200px 
     if (width > 1200){
         timeLightingTwo();
+        continueScroll();
     }
 
     // Cheching light on 1400px 
     if (width > 1400){
         timeLightingThree();
+        continueScroll();
     }
     // 2500px
     if (width > 2500){
         lamp.style.top = `${calcul -300}px`;
         checkScreen2();
         timeLightingSuperLarge();
+        continueScrollX();
+        yellowCheckX();
     }
     // 2880PX
     if (width > 2800){
         lamp.style.top = `${calcul -300}px`;
         checkScreen3();
         timeLightingSuperLargeX();
+        continueScrollX2();
+        yellowCheckX2();
     }
-
-    console.log(width)
 
 });
 
+
+/*** CONTINUE SCROLL ***/
+function continueScroll(){
+    scroll = window.pageYOffset
+
+    if(scroll > 1200){
+        lampDark.style.top = `${scroll -1100}px`
+        lightTime.style.display = `none`;
+        shadowLight.style.display = `none`;
+        
+        console.log('work!!!!!!CS..')
+    }
+}
+//***CONTINUE S FOR 2500PX***//
+function continueScrollX(){
+    scroll = window.pageYOffset
+
+    if(scroll > 1400){
+        lampDark.style.top = `${scroll -1300}px`
+        lightTime.style.display = `none`;
+        shadowLight.style.display = `none`;
+        
+        console.log('work!!!!!!CS 2500px..')
+    }
+}
+//***CONTINUE S FOR 2880PX***//
+function continueScrollX2(){
+    scroll = window.pageYOffset
+
+    if(scroll > 1800){
+        lampDark.style.top = `${scroll -1300}px`
+        lightTime.style.display = `none`;
+        shadowLight.style.display = `none`;
+        
+        console.log('work!!!!!!CS 2500px..')
+    }
+}
+/*** YELLOW LAMP 2500px ***/
+function yellowCheckX(){
+
+    const width = showcaseWidth.offsetWidth
+    const scroll = window.pageYOffset
+
+    if(scroll > 2280 && width < 2800){
+        yellowLamp.style.display = `block`;
+        yellowLamp.style.top = `${scroll -2040}px`;
+        console.log('THAT HAPPEND 2500px!!')
+     }
+}
+/*** YELLOW LAMP 2880px ***/
+function yellowCheckX2(){
+
+    scroll = window.pageYOffset
+
+    if(scroll > 3000){
+        yellowLamp.style.display = `block`;
+        yellowLamp.style.top = `${scroll -2150}px`;
+        console.log('THAT HAPPEND 2880px!!')
+     }
+}  
+/*** YELLOW LAMP ***/
+function yellowCheck(){
+
+    const scroll = window.pageYOffset
+    const width = showcaseWidth.offsetWidth
+
+    if(scroll > 1790 && width < 2000){
+        yellowLamp.style.display = `block`;
+        yellowLamp.style.top = `${scroll -1980}px`;
+        console.log('THAT HAPPEND!!')
+     }
+} 
 
 //***Functions for Light***///
 function checkScreen(){
@@ -91,11 +169,29 @@ function checkScreen(){
   
     let calcul = scroll - 500
     if (scroll > 650 & width > 890 ) {
-        lamp.style.display = `none`; 
+        lamp.style.display = `none`;
+        yellowLamp.style.display = `none`; 
         lampDark.style.top =  `90px`; 
-        console.log('Disper Grey!')
+        console.log('Disper Grey! First!')
     } else {
-        lamp.style.display = `block`; 
+        lamp.style.display = `block`;
+        yellowLamp.style.display = `block`; 
+        console.log('Grey Is Back! First!');
+}}
+//** 1200 CHECK */
+function checkScreen12(){
+    let scroll = window.pageYOffset
+    let width = showcaseWidth.offsetWidth
+  
+    let calcul = scroll - 500
+    if (scroll > 800 & width < 1400 ) {
+        lamp.style.display = `none`;
+        yellowLamp.style.display = `none`; 
+        lampDark.style.top =  `${scroll -500}px`; 
+        console.log('Disper Grey! 1200px')
+    } else {
+        lamp.style.display = `block`;
+        yellowLamp.style.display = `block`; 
         console.log('Grey Is Back!');
 }}
 //**checking on 2500px **//
@@ -104,9 +200,10 @@ function checkScreen2(){
     let width = showcaseWidth.offsetWidth
   
     let calcul = scroll - 500
-    if (scroll > 850 & width > 2400 ) {
-        lamp.style.display = `none`; 
-        lampDark.style.top =  `700px`; 
+    if (scroll > 850 && width > 2400 ) {
+        lamp.style.display = `none`;
+        lampDark.style.display = `block`; 
+        lampDark.style.top =  `-70px`; 
         console.log('Disper Grey22!')
     } else {
         lamp.style.display = `block`; 
@@ -117,8 +214,7 @@ function checkScreen3(){
     let scroll = window.pageYOffset
     let width = showcaseWidth.offsetWidth
   
-    let calcul = scroll - 500
-    if (scroll > 1150 & width > 2800 ) {
+    if (scroll > 1000 & width > 2800 ) {
         lamp.style.display = `none`; 
         lampDark.style.top =  `700px`; 
         console.log('Disper Grey33!')
@@ -133,7 +229,7 @@ function timeLighting(){
     let width = showcaseWidth.offsetWidth
     let calcul = scroll - 300
 
-    if (scroll >= 900 ) { 
+    if (scroll >= 900 && width < 1000 ) { 
       lampDark.style.top = `20px`;
       lightTime.style.transform = `block`;
       lightTime.style.display = `block`;
@@ -154,10 +250,11 @@ function timeLightingTwo(){
     let width = showcaseWidth.offsetWidth
     let calcul = scroll - 300
 
-    if (scroll >= 900 & width > 1199) {
-        lampDark.style.top = `40px`
+    if (scroll >= 900 & width < 1400) {
+        lampDark.style.top = `70px`
         lightTime.style.display = `block`;
         lampDark.style.display = `block`;
+        shadowLight.style.display = `block`;
         console.log('LIGHT IS ON 1200px!');
         console.log('scroll is:', scroll)
     } else {
@@ -175,8 +272,8 @@ function timeLightingThree(){
         let width = showcaseWidth.offsetWidth
         let calcul = scroll - 300
     
-        if (scroll >= 900 & width > 1300) {
-            lampDark.style.top = `20px`
+        if (scroll >= 900 & width > 1400) {
+            lampDark.style.top = `55px`
             lightTime.style.transform = `block`;
             lightTime.style.display = `block`;
             console.log('LIGHT IS ON 1400px!');
@@ -194,8 +291,8 @@ function timeLightingSuperLarge(){
     let width = showcaseWidth.offsetWidth
     let calcul = scroll - 300
 
-    if (scroll >= 1230 & width > 2500) { 
-        lampDark.style.top = `80px`
+    if (scroll >= 1200 & width > 2500) { 
+        lampDark.style.top = `60px`
         lightTime.style.transform = `block`;
         lightTime.style.display = `block`;
         shadowLight.style.display = `block`
@@ -215,8 +312,8 @@ function timeLightingSuperLargeX(){
     let width = showcaseWidth.offsetWidth
     let calcul = scroll - 300
 
-    if (scroll >= 1700 & width > 2800) { 
-        lampDark.style.top = `500px`
+    if (scroll >= 1500 & width > 2800) { 
+        lampDark.style.top = `300px`
         console.log(scroll)
         lightTime.style.transform = `block`;
         lightTime.style.display = `block`;
